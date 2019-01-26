f5088ea9303175cad648b616e6a8cac8d47523b8
Paul-Cl-ark <clark.paul123@gmail.com>
2019-01-26 19:36:47 +0000

    done

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,16 @@
 export default (state = [], action) => {
-  return state;
+	switch (action.type) {
+		case 'ADD_QUOTE':
+			return [...state, action.quote]
+		case 'REMOVE_QUOTE':
+			return state.filter(quote => quote.id !== action.quoteId)
+		case 'UPVOTE_QUOTE':
+			return [...state, ...(state[state.indexOf(action.id) + 1].votes += 1)]
+		case 'DOWNVOTE_QUOTE':
+			return state[state.indexOf(action.id) + 1].votes > 0
+				? [...state, ...(state[state.indexOf(action.id) + 1].votes -= 1)]
+				: state
+		default:
+			return state
+	}
 }
